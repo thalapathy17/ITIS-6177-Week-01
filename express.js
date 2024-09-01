6901aec0e7f54cd2c84bd11e0f5620c1d6ec1c76
thalapathy17 <abhishekbhure8@gmail.com>
2024-09-01 17:37:07 -0400

    changed port and added gitignore

--- express.js
+++ express.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
  app.get('/', (req,res) => {
-    res.send('hello worls!')
+    res.send('hello world!')
  })
 
  app.listen ( port, () => {
